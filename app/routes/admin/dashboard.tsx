96f60219f53a536c843021bf0dca4720a94d54e7
bindupatel-19 <bindugombe199@gmail.com>
2025-08-02 20:58:12 +0530

    Display created trips on dashboard using TripCard component

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -31,9 +31,26 @@ const Dashboard = () => {
                         lastMonthCount={userRole.lastMonth}
                     />
                 </div>
-                </section>
+            </section>
 
-            <TripCard/>
+
+               <section className="container">
+                    <h1 className="text-xl font-semibold text-dark-100"> Created Trips</h1>
+
+                   <div className="trip-grid">
+                       {allTrips.slice(0, 4).map(({ id, name, imageUrls, itinerary, tags, estimatedPrice }) => (
+                           <TripCard
+                                key={id}
+                                id={id.toString()}
+                                name={name}
+                                imageUrl={imageUrls[0]}
+                                location={itinerary?.[0]?.location ?? ""}
+                                tags={tags}
+                                price={estimatedPrice}
+                           />
+                       ))}
+                   </div>
+               </section>
         </main>
     )
 }
